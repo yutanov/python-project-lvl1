6a5e5c838106899c9a9a8d8a26c5c162231d23cb
yutanov <utanov@gmail.com>
2021-07-17 11:41:10 +0300

    brain_even

--- Makefile
+++ Makefile
@@ -10,4 +10,6 @@ package-install:
 	python3 -m pip install --user dist/*.whl
 lint:
 	poetry run flake8 brain_games
+brain-even:
+	poetry run brain-even
 
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import random
+import prompt
+
+
+def get_answer():
+    number = int(random.randint(1,100))
+    print('Question: ', number)
+    answer = str(input('Your answer: '))
+    return number, answer
+
+
+def is_even(number):
+    if number % 2 == 0:
+        return('yes')
+    else:
+        return('no')
+
+
+def right_answer():
+    print('Correct!')
+
+
+def wrong_answer(name, answer, check_answer):
+    print("'{}' is wrong answer ;(. Correct answer was '{}'.".format(answer, check_answer))
+    print("Let's try again, {}!".format(name))
+
+
+def congratilations(name):
+    print("Congratilations, {}!".format(name))
+
+
+def game(name):
+    for i in range(3):
+        number, answer = get_answer()
+        check_answer = is_even(number)
+        if check_answer == answer:
+            right_answer()
+        else:
+            wrong_answer(name, answer, check_answer)
+            break
+    else:    
+        congratilations(name)
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print('Hello, {}!'.format(name))
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    game(name)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
