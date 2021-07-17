46ee9eb949ef0be71cbf9d35358d998a654cc950
yutanov <utanov@gmail.com>
2021-07-18 02:26:26 +0300

    linted cli.py brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -24,7 +24,7 @@ def right_answer():
 
 def wrong_answer(name, answer, check_answer):
     print("'{}' is wrong answer ;(."
-    "Correct answer was '{}'.".format(answer, check_answer))
+          "Correct answer was '{}'.".format(answer, check_answer))
     print("Let's try again, {}!".format(name))
 
 
