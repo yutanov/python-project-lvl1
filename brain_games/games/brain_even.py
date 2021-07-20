1ff45e1038cd73492587a3038e3a9cdcdb5cde2a
yutanov <86551782+yutanov@users.noreply.github.com>
2021-07-20 19:00:36 +0300

    Update brain_even.py

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -6,14 +6,14 @@ COND = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def is_even(number):
     if number % 2 == 0:
-        return('yes')
+        return "yes"
     else:
-        return('no')
+        return "no"
 
 
 def get_answer():
     number = int(random.randint(1, 100))
-    print('Question: ', number)
+    print("Question: ", number)
     correct_answer = is_even(number)
-    answer = str(input('Your answer: '))
+    answer = str(input("Your answer: "))
     return correct_answer, answer
